85a8005804034fcb35777826fc41ac79fca208a2
mlsandoval <michaelsandoval8@gmail.com>
2020-01-23 06:42:09 +0000

    base form for addTodo complete, need to add callbacks to send todo up to main app, then down to the todolist

--- client/components/TodoApp.jsx
+++ client/components/TodoApp.jsx
@@ -7,11 +7,27 @@ import TodoList from './todoList.jsx'
 import VisibilityFilters from './visibilityFilters.jsx'
 
 class TodoApp extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      todos : []
+    }
+
+    this.addTodo = this.addTodo.bind(this)
+  }
+
+  addTodo(todo){
+    let todos = this.state.todos.map();
+    todos.push(todo)
+
+    this.setState({todos})
+  }
+
   render () {
     return(
       <div className="main">
         <Header></Header>
-        <AddTodo></AddTodo>
+        <AddTodo addTodoCallback={this.addTodo}></AddTodo>
         <TodoList></TodoList>
         <VisibilityFilters></VisibilityFilters>
       </div>
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class Header extends React.Component {
+ 
   render () {
-    
+    <h4>Header</h4>
   }
 }
